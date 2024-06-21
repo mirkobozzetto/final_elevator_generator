5a22d0ed3dcce538f68935a36161cc13be7f147a
Mirko Bozzetto <mirkouch@gmail.com>
2024-06-21 10:15:10 +0200

    handlefilechangewrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,10 @@ const handleFileChangeWrapper = (e: FileChangeEvent) => {
 export default function Home() {
   const [image, setImage] = useState<ImageState | undefined>(undefined);
 
+  const handleFileChangeWrapper = (e: FileChangeEvent) => {
+    handleFileChange(e, setImage);
+  };
+
   return (
     <main className="flex lg:flex-row flex-col justify-center items-center gap-8 m-auto max-w-4xl min-h-full">
       <div className="bg-indigo-100 shadow-xl w-96 card">
